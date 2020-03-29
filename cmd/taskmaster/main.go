42f06c34b46024f45afebf4f7f4462d183b3c9cf
Miikka Tuominen <tuommii@gmail.com>
2020-03-30 02:09:39 +0300

    added stuff to readme

--- cmd/taskmaster/main.go
+++ cmd/taskmaster/main.go
@@ -8,6 +8,45 @@ import (
 	"miikka.xyz/linedit"
 )
 
+// Reset all custom styles
+const resetAll = "\033[0m"
+
+// Reset to default color
+const resetColor = "\033[32m"
+
+// Clear line and put cursor at beginning of it
+const resetLine = "\r\033[K"
+
+// Clear screen
+func Clear() {
+	fmt.Printf("\033[2J")
+}
+
+// MoveCursor to given position
+func MoveCursor(x int, y int) {
+	fmt.Printf("\033[%d;%dH", y, x)
+}
+
+// MoveCursorForward relative the current position
+func MoveCursorForward(bias int) {
+	fmt.Printf("\033[%dC", bias)
+}
+
+// MoveCursorBackward ...
+func MoveCursorBackward(bias int) {
+	fmt.Printf("\033[%dD", bias)
+}
+
+// Move cursor down relative the current position
+func MoveCursorDown(bias int) {
+	fmt.Printf("\033[%dB", bias)
+}
+
+// Move cursor up relative the current position
+func MoveCursorUp(bias int) {
+	fmt.Printf("\033[%dA", bias)
+}
+
 func readKey(reader *bufio.Reader) (rune, int) {
 	char, size, err := reader.ReadRune()
 	if err != nil {
@@ -15,6 +54,8 @@ func readKey(reader *bufio.Reader) (rune, int) {
 	}
 	return char, size
 }
+
+var str = ""
 
 func main() {
 	backup, _ := linedit.Attr(os.Stdin)
@@ -25,6 +66,8 @@ func main() {
 
 	var b []byte = make([]byte, 5)
 
+	Clear()
+	// MoveCursor(0, 0)
 	for {
 		n, _ := os.Stdin.Read(b)
 		code := 0
@@ -33,11 +76,21 @@ func main() {
 		}
 		if code == 'x' {
 			break
-		} else if int(b[0]) >= int('A') && int(b[0]) < int('z') {
-			fmt.Print(string(code))
+		} else if int(b[0]) >= int('A') && int(b[0]) < int('z') && code != 'y' {
+			fmt.Print(resetLine)
+			str += string(code)
+			fmt.Print(str)
 		} else if code >= 183 && code <= 186 {
 			// cursor.MoveRight(10)
-			fmt.Print(string(b))
+			fmt.Print(resetLine)
+			MoveCursorForward(10)
+			// fmt.Print(string(b))
+		} else if code == 'y' {
+			MoveCursor(0, 0)
+		} else if code == 13 {
+			MoveCursorDown(1)
+			str = ""
+			fmt.Print(resetLine)
 		} else {
 			fmt.Println(code)
 		}
