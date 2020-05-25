d7ae0924feebb5ac081c9275b7a7bd283e158c48
Miikka Tuominen <tuommii@gmail.com>
2020-05-25 12:15:36 +0300

    more refactoring

--- job/process.go
+++ job/process.go
@@ -46,7 +46,45 @@ func LoadAll(path string) map[string]*Process {
 	return tasks
 }
 
-// Redirects standard stream to file. If path aint valid, then using alternative.
+// Launch ...
+func (p *Process) Launch() {
+	p.prepare()
+	go p.redirect(p.stdout, p.OutputLog, os.Stdout)
+	go p.redirect(p.stderr, p.ErrorLog, os.Stderr)
+	p.Cmd.Start()
+	go p.clean()
+}
+
+// clean process when ready
+func (p *Process) clean() {
+	// Wait until process is done
+	err := p.Cmd.Wait()
+	if err != nil {
+		fmt.Println(err)
+	}
+	// No need to call Close() when using pipes ?
+	// p.stdout.Close()
+	// p.stderr.Close()
+}
+
+// prepare command for executiom
+func (p *Process) prepare() {
+	tokens := strings.Fields(p.Command)
+	p.Cmd = exec.Command(tokens[0], tokens[1:]...)
+
+	var err error
+	// TODO: err checks
+	p.stdout, err = p.Cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("PIPE:", err)
+	}
+	p.stderr, err = p.Cmd.StderrPipe()
+	if err != nil {
+		fmt.Println("PIPE", err)
+	}
+}
+
+// redirect standard stream to file. If path wasn't valid, then using alternative.
 func (p *Process) redirect(stream io.ReadCloser, path string, alternative *os.File) {
 	s := bufio.NewScanner(stream)
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_APPEND|os.O_WRONLY, 0644)
@@ -57,71 +95,10 @@ func (p *Process) redirect(stream io.ReadCloser, path string, alternative *os.Fi
 	for s.Scan() {
 		fmt.Fprintln(file, s.Text())
 	}
-	// When ReadCloser is closed this will executed
-	fmt.Println(p.Name, "stopped reading")
-}
-
-// Launch ...
-func (p *Process) Launch() {
-	tokens := strings.Fields(p.Command)
-	p.Cmd = exec.Command(tokens[0], tokens[1:]...)
-
-	// TODO: err checks
-	p.stdout, _ = p.Cmd.StdoutPipe()
-	p.stderr, _ = p.Cmd.StderrPipe()
-	go p.redirect(p.stdout, p.OutputLog, os.Stdout)
-	go p.redirect(p.stderr, p.ErrorLog, os.Stderr)
-
-	// go func() {
-	// 	s := bufio.NewScanner(p.stdout)
-	// 	file, err := os.OpenFile(p.StdoutLog, os.O_CREATE|os.O_TRUNC|os.O_APPEND|os.O_WRONLY, 0644)
-	// 	if err != nil {
-	// 		file = os.Stdout
-	// 		fmt.Println("FILE OPEN ERROR", err)
-	// 	}
-	// 	for s.Scan() {
-	// 		fmt.Fprintln(file, s.Text())
-	// 	}
-	// 	// When ReadCloser is closed this will executed
-	// 	fmt.Println(p.Name, "stopped reading")
-	// }()
-
-	// errFile := p.GetStderr()
-	// if errFile != nil {
-	// 	defer errFile.Close()
-	// }
-
-	// outFile := p.GetStdout()
-	// if outFile != nil {
-	// 	defer outFile.Close()
-	// }
-
-	p.Cmd.Start()
-	go func() {
-		// Wait until process is done
-		err := p.Cmd.Wait()
-		if err != nil {
-			fmt.Println(err)
-		}
-		p.stdout.Close()
-		p.stderr.Close()
-	}()
-	// p.Done <- true
-	// stdout, err := p.Cmd.StdoutPipe()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// buf := bufio.NewReader(stdout) // Notice that this is not in a loop
-	// num := 1
-	// for {
-	// 	line, _, err := buf.ReadLine()
-	// 	if err != nil {
-	// 		return
-	// 	}
-	// 	// if num > 3 {
-	// 	// 	return
-	// 	// }
-	// 	num++
-	// 	fmt.Println(string(line))
-	// }
+	// When stream is closed this will executed
+	which := "STDOUT"
+	if stream == p.stderr {
+		which = "STDERR"
+	}
+	fmt.Println(p.Name, which, "stopped")
 }
