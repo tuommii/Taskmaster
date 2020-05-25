2298b614d0d4b8e52ca2b92ba38c8700b40f4223
Miikka Tuominen <tuommii@gmail.com>
2020-05-25 19:02:06 +0300

    simple reloading without safety checks

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -21,7 +21,7 @@ func main() {
 	flag.Parse()
 	tasks := job.LoadAll(*configPath)
 
-	listenSignals()
+	listenSignals(*configPath, tasks)
 
 	for key, task := range tasks {
 		fmt.Println(key, task)
@@ -31,28 +31,40 @@ func main() {
 	listenConnections()
 }
 
-func listenSignals() {
+func listenSignals(configPath string, tasks map[string]*job.Process) {
 	// We must use a buffered channel or risk missing the signal
 	// if we're not ready to receive when the signal is sent
 	signalsCh := make(chan os.Signal, 1)
 	// Passing no signals to Notify means that all
 	// signals will be sent to the channel.
 	signal.Notify(signalsCh)
-	go signalHandler(signalsCh)
+	// go signalHandler(signalsCh)
+	go func() {
+		for s := range signalsCh {
+			switch {
+			case s == syscall.SIGHUP:
+				fmt.Println("RELOAD")
+				for key, task := range tasks {
+					fmt.Println("Killing and deleting", key)
+					task.Kill()
+					delete(tasks, key)
+				}
+				tasks = job.LoadAll(configPath)
+				for _, task := range tasks {
+					task.Launch()
+				}
+				fmt.Println("LOADED", len(tasks), "TASKS")
+			case s == syscall.SIGTERM || s == syscall.SIGINT:
+				fmt.Printf("\nABORT!")
+				os.Exit(0)
+			default:
+			}
+		}
+	}()
 }
 
-func signalHandler(signalsCh chan os.Signal) {
-	for s := range signalsCh {
-		switch {
-		case s == syscall.SIGHUP:
-			fmt.Println("RELOAD")
-		case s == syscall.SIGTERM || s == syscall.SIGINT:
-			fmt.Printf("\nABORT!")
-			os.Exit(0)
-		default:
-		}
-	}
-}
+// func signalHandler(signalsCh chan os.Signal) {
+// }
 
 func listenConnections() {
 	l, err := net.Listen("tcp", ":4200")
