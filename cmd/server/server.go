a6869d2bc4579bf205b18910c4d5d8601d29ac77
Miikka Tuominen <tuommii@gmail.com>
2020-05-26 16:12:35 +0300

    ok solution for now

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -16,22 +16,37 @@ import (
 	"golang.org/x/net/netutil"
 )
 
+type server struct {
+	configPath string
+	tasks      map[string]*job.Process
+}
+
+func newServer(configPath string, tasks map[string]*job.Process) *server {
+	s := &server{
+		configPath: configPath,
+		tasks:      tasks,
+	}
+	return s
+}
+
+func (s *server) launchAll() {
+	for key, task := range s.tasks {
+		fmt.Println(key, task)
+		task.Launch()
+	}
+}
+
 // reloader func, takes path to config and old processes
 type reloaderF func(string, map[string]*job.Process)
 
 func main() {
 	configPath := flag.String("config", "./config.example.json", "path to config file")
 	flag.Parse()
-	tasks := job.LoadAll(*configPath)
-
-	listenSignals(*configPath, tasks)
-
-	for key, task := range tasks {
-		fmt.Println(key, task)
-		task.Launch()
-	}
-
-	listenConnections()
+	// tasks := job.LoadAll(*configPath)
+	s := newServer(*configPath, job.LoadAll(*configPath))
+	s.listenSignals()
+	s.launchAll()
+	s.listenConnections()
 }
 
 func reloadConfig(configPath string, tasks map[string]*job.Process) {
@@ -48,7 +63,7 @@ func reloadConfig(configPath string, tasks map[string]*job.Process) {
 	// fmt.Println("LOADED", len(tasks), "TASKS")
 }
 
-func listenSignals(configPath string, tasks map[string]*job.Process) {
+func (s *server) listenSignals() {
 	// We must use a buffered channel or risk missing the signal
 	// if we're not ready to receive when the signal is sent
 	signalsCh := make(chan os.Signal, 1)
@@ -56,33 +71,21 @@ func listenSignals(configPath string, tasks map[string]*job.Process) {
 	// signals will be sent to the channel.
 	signal.Notify(signalsCh)
 
-	go signalHandler(signalsCh)
-}
-
-func signalHandler(signalsCh chan os.Signal) {
-	for s := range signalsCh {
-		switch {
-		case s == syscall.SIGHUP:
-			fmt.Println("RELOAD")
-			// for key, task := range tasks {
-			// 	fmt.Println("Killing and deleting", key)
-			// 	task.Kill()
-			// 	delete(tasks, key)
-			// }
-			// tasks = job.LoadAll(configPath)
-			// for _, task := range tasks {
-			// 	task.Launch()
-			// }
-			// fmt.Println("LOADED", len(tasks), "TASKS")
-		case s == syscall.SIGTERM || s == syscall.SIGINT:
-			fmt.Printf("\nABORT!")
-			os.Exit(0)
-		default:
+	go func(tasks map[string]*job.Process) {
+		for s := range signalsCh {
+			switch {
+			case s == syscall.SIGHUP:
+				fmt.Println("RELOAD", len(tasks))
+			case s == syscall.SIGTERM || s == syscall.SIGINT:
+				fmt.Printf("\nABORT!")
+				os.Exit(0)
+			default:
+			}
 		}
-	}
+	}(s.tasks)
 }
 
-func listenConnections() {
+func (s *server) listenConnections() {
 	l, err := net.Listen("tcp", ":4200")
 	if err != nil {
 		log.Fatal("LISTEN:", err)
