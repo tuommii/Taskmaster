4678278cb3f19a29ada9184e5bb838bcc05d3396
Miikka Tuominen <tuommii@gmail.com>
2020-05-30 21:09:39 +0300

    option flag to daemonize

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
+	"github.com/sevlyar/go-daemon"
 	"github.com/tuommii/taskmaster/job"
 	"github.com/tuommii/taskmaster/logger"
 )
 
+func daemonize() {
+	cntxt := &daemon.Context{
+		PidFileName: "sample.pid",
+		PidFilePerm: 0644,
+		LogFileName: "sample.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon sample]"},
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatal("Unable to run: ", err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+}
+
 func main() {
 	configPath := flag.String("config", "./config.example2.json", "path to config file")
-	// daemonFlag := flag.Bool("d", false, "run as a daemon")
+	daemonFlag := flag.Bool("d", false, "daemonize")
 	_ = logger.Get()
 	log.Print("Systemlog test")
 	flag.Parse()
@@ -33,6 +56,27 @@ func main() {
 	// 	return
 	// }
 	// defer cntxt.Release()
+	if *daemonFlag {
+		fmt.Println("DAEMON")
+		cntxt := &daemon.Context{
+			PidFileName: "sample.pid",
+			PidFilePerm: 0644,
+			LogFileName: "sample.log",
+			LogFilePerm: 0640,
+			WorkDir:     "./",
+			Umask:       027,
+			Args:        []string{"[go-daemon sample]"},
+		}
+
+		d, err := cntxt.Reborn()
+		if err != nil {
+			log.Fatal("Unable to run: ", err)
+		}
+		if d != nil {
+			return
+		}
+		defer cntxt.Release()
+	}
 
 	s := newServer(*configPath, job.LoadAll(*configPath))
 	s.listenSignals()
